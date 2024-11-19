22610f7454476b925c8be6ec94b64407b1b918f9
Carlos Fernandez <dominguezalacid@gmail.com>
2024-11-19 19:49:39 +0100

    actualizar

--- navigation/DrawerNavigation.js
+++ navigation/DrawerNavigation.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { createDrawerNavigator } from "@react-navigation/drawer";
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import TabNavigation from "./TabNavigation";
 import SettingsScreen from "../screens/SettingsScreen";
 
@@ -7,9 +8,17 @@ const Drawer = createDrawerNavigator();
 
 export default function DrawerNavigation() {
     return (
-        <Drawer.Navigator>
-            <Drawer.Screen name="Home" component={TabNavigation} />
-            <Drawer.Screen name="Settings" component={SettingsScreen} />
+        <Drawer.Navigator
+            drawerContentOptions={{ 
+                activeTintColor: "#e91e63" 
+            }}
+        >
+            <Drawer.Screen name="Home" component={TabNavigation} 
+                options={{ drawerIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} />) }}
+            />
+            <Drawer.Screen name="Settings" component={SettingsScreen}
+                options={{ drawerIcon: ({ color, size }) => (<MaterialCommunityIcons name="cog" color={color} size={size} />) }} 
+            />
         </Drawer.Navigator>
     );
 }
--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import HomeScreen from '../screens/HomeScreen';
 import AddTaskScreen from '../screens/AddTaskScreen';
 
@@ -7,9 +8,27 @@ const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
     return (
-        <Tab.Navigator>
-            <Tab.Screen name="Tasks" component={HomeScreen} />
-            <Tab.Screen name="Add Task" component={AddTaskScreen} />
+        <Tab.Navigator
+            screenOptions={{
+                activeTintColor: '#e91e63',
+            }}
+        >
+            <Tab.Screen name="Tasks" component={HomeScreen}
+                options={{
+                    tabBarLabel: 'Tasks',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="format-list-bulleted" color={color} size={size} />
+                    ),
+                }}
+            />
+            <Tab.Screen name="Add Task" component={AddTaskScreen}
+                options={{
+                    tabBarLabel: 'Add Tasks',
+                    tabBarIcon: ({ color, size }) => (
+                        <MaterialCommunityIcons name="plus" color={color} size={size} />
+                    ),
+                }}
+            />
         </Tab.Navigator>
     );
 }
